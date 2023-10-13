94fbbf8414837361b35fc1ec551af6c424f01fcf
hamdanmajid125 <hamdanmajid456@gmail.com>
2023-10-14 02:08:34 +0500

    add chat

--- pages/_app.js
+++ pages/_app.js
@@ -31,12 +31,22 @@ export default function App({ Component, pageProps }) {
     });
   };
 
-
-
   return (
 
     <>
-      <Zendesk defer zendeskKey={ZENDESK_KEY} onLoaded={handleLoaded} />
+      {/* <Zendesk defer zendeskKey={ZENDESK_KEY} onLoaded={handleLoaded} /> */}
+      <Script id="show-banner">
+  {`var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6529a80d6fcfe87d54b99936/1hclbgtvm';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();`}
+</Script>
+
 
       <Head>
 
